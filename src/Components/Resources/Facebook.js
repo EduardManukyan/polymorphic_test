aec4edc33fa797d4f5fb96bdb8fe8c4ab9ad9be8
Eduard Manukyan <edman110296@gmail.com>
2021-04-23 21:38:54 +0400

    install convert-svg, add folder assets and resources, style changes

---
+++ src/Components/Resources/Facebook.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+function SvgFacebook(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={8.021}
+      height={15.968}
+      {...props}
+    >
+      <path
+        d="M5.958 3.04h2.063V.001H5.597v.01c-2.936.1-3.539 1.752-3.592 3.483h-.006v1.517h-2v2.978h2v7.98h3.012v-7.98H7.48l.476-2.978H5.011v-.917a.981.981 0 01.947-1.054z"
+        fill="#999"
+      />
+    </svg>
+  );
+}
+
+export default SvgFacebook;
---
+++ src/Components/Resources/index.js
@@ -0,0 +1,6 @@
+export { default as Behance } from './Behance'
+export { default as Facebook } from './Facebook'
+export { default as GooglePlus } from './GooglePlus'
+export { default as Instagram } from './Instagram'
+export { default as Pinterest } from './Pinterest'
+export { default as Twitter } from './Twitter'
